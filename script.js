b959281c9a42d8c76ae1c6d842829a2eea8e145f
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-04-05 14:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+
+const form = document.querySelector("form")  
+function getCookie(name) {
+  const cookies = document.cookie.split("; ");
+  
+  for (let c of cookies) {
+    const [key, value] = c.split("=");
+    if (key === name) return value;
+  }
+  return null;
+}
+window.onload=function(){ 
+	// const cookies=document.cookie.split(";")
+	const fontsize = getCookie("fontsize"); 
+	const fontcolor = getCookie("fontcolor"); 
+	  if (fontsize) {
+	    document.body.style.fontSize = fontsize + "px";
+	  }
+	
+	  if (fontcolor) {
+	    document.body.style.color = fontcolor;
+	  }
+}
+form.addEventListener('submit',(e)=>{ 
+	e.preventDefault()
+	const fontsize = document.getElementById("fontsize").value
+	const fontcolor = document.getElementById("fontcolor").value
+	document.body.style.fontSize= fontsize+"px"
+	document.body.style.color= fontcolor
+  document.cookie = `fontsize=${fontsize}; path=/`;
+  document.cookie = `fontcolor=${fontcolor}; path=/`;
+})
